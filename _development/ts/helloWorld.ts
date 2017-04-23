f6fa575a5afbf0b51308d7824b545ac1ba185bfd
Ross Lehr <itsme@rosslehr.com>
2017-04-22 20:14:10 -0400

    added TypeScript

---
+++ _development/ts/helloWorld.ts
@@ -0,0 +1,10 @@
+class Greeter {
+    constructor(public greeting: string) { }
+    greet() {
+        return "<h1>" + this.greeting + "</h1>";
+    }
+};
+
+var greeter = new Greeter("Hello, world its me a test!");
+    
+document.body.innerHTML = greeter.greet();
